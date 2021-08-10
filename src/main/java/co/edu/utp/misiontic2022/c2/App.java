47ca2b1410950b6de5978a46f775eefd3a951833
Luz Gomez <luz.torrente2020@gmail.com>
2021-08-09 22:16:27 -0500

    Se realiza conexion a  ok

--- src/main/java/co/edu/utp/misiontic2022/c2/App.java
+++ src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -1,9 +1,22 @@
 package co.edu.utp.misiontic2022.c2;
 
+import java.sql.SQLException;
+
+import org.sqlite.jdbc3.JDBC3Connection;
+
+import co.edu.utp.misiontic2022.c2.util.JDBCUtilities;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            var conectar = JDBCUtilities.getConnection();
+            System.out.println("Conexion con BD ok");
+            conectar.close();
+        } catch (SQLException e) {
+            System.err.println("Error " + e);
+            e.printStackTrace();
+        }
     }
 }
